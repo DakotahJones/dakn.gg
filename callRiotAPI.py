483f663acef068f4c6d3593ceb5927b4e266dc08
Dakotah Jones <dakmasterr@gmail.com>
2025-01-06 10:59:42 -0500

    Updated .gitignore and callRiotAPI.py callRiotAPI.py now uses the Riot API key from the config.py file callRiotAPI.py now returns the JSON response from the Riot API

--- callRiotAPI.py
+++ callRiotAPI.py
@@ -1,5 +1,10 @@
 from config import api_key, api_url
 
-import requests
+import requests # import requests module
 
-requests.get(api_url)
+response = requests.get(api_url, headers={"X-Riot-Token": api_key}) # get request to api_url with api_key
+player_info = (response.json())
+player_accountID = player_info['accountId'] # get accountID from player_info
+
+print(player_info) # prints ID, accountID, puuid, name, profileIconId, revisionDate, summonerLevel
+print(player_accountID) # prints accountID
